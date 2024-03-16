fb31c7e9bda35f194293125b11242b3d37e36ebe
Weiss Jiang <meweissjiang@gmail.com>
2024-03-16 15:58:27 +1100

    updated login page

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,14 +1,59 @@
 import './Login.scss';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { loginUser } from '../../store/auth/action';
+import { setToast } from '../../store/toast/action';
+import { useNavigate } from 'react-router-dom';
+
 export default function Login() {
+    const navigate = useNavigate();
+
+    const dispatch = useDispatch();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("login starts")
+        dispatch(loginUser({ email, password }))
+            .then((res) => {
+                navigate('/');
+                console.log("res:", res)
+            })
+            .catch((e) => {
+                dispatch(setToast({ message: e.toString(), type: "error" }));
+            });
+
+    };
     return (
        <div className='login-page'>
             <div className='box'>
                 <div className='login'>
                     <div className='loginBx'>
                         <h2>Login</h2>
-                        <input type='text' placeholder='Email'></input>
-                        <input type='password' placeholder='Password'></input>
-                        <input type='submit' placeholder='Sign in'></input>
+                        <input 
+                            type='text' 
+                            id="email" 
+                            name="email"
+                            autocomplete="email"
+                            placeholder='Email' 
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        ></input>
+                        <input 
+                            type='password' 
+                            id="password" 
+                            name="password"
+                            autocomplete="current-password"
+                            placeholder='Password' 
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        ></input>
+                        <input 
+                            type='submit' 
+                            placeholder='Sign in'
+                            onClick={handleSubmit}
+                        ></input>
                         <div className='group'>
                             <a href='https://www.linkedin.com/in/weiss-jiang/'>Forget Password</a>
                             <a href='https://www.linkedin.com/in/weiss-jiang/'>Register</a>
